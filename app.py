1208af587196bc0cc82559a69ac921d856cccab0
Rolex Omondi <rolexomondi46@gmail.com>
2023-09-09 11:52:53 +0300

    finished the endpoint

--- app.py
+++ app.py
@@ -10,9 +10,25 @@ def get_info():
     track = request.args.get('track')
 
     #getting the current day of the week
-    current_date = datetime.datetime.now().strftime('%A')
+    current_day = datetime.datetime.now().strftime('%A')
 
     #getting the utc time
     utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    github_file_url = 
+    github_file_url = "https://github.com/Rolex46/endpoint/blob/master/app.py"
+    github_repo_url = "https://github.com/Rolex46/endpoint"
+
+    response_data = {
+         "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
